49331c99a9893bd450fd16fb6754fed72bc628ae
Michael Kopp <michael.kopp2@de.bosch.com>
2024-11-18 11:18:03 +0100

    show two numbers with switched digits

--- src/app.rs
+++ src/app.rs
@@ -1,12 +1,19 @@
 use yew::prelude::*;
+use rand::seq::IteratorRandom;
 
 #[function_component(App)]
 pub fn app() -> Html {
+    let mut rng = rand::thread_rng();
+    // ensure that we do not get the same digit twice, e.g. 77
+    let digits = (0..10).choose_multiple(&mut rng, 2);
+    let number1 = 10 * digits[0] + digits[1];
+    let number2 = 10 * digits[1] + digits[0];
+
     html! {
         <main>
-            <img class="logo" src="https://yew.rs/img/logo.png" alt="Yew logo" />
-            <h1>{ "Hello World!" }</h1>
-            <span class="subtitle">{ "from Yew with " }<i class="heart" /></span>
+            <h1>{ "Welche Zahl ist größer?" }</h1>
+            <p>{ number1.to_string() }</p>
+            <p>{ number2.to_string() }</p>
         </main>
     }
 }
