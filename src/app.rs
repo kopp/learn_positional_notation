46fc3997454e5b912bc6a338c353db09fa640ad9
Michael Kopp <michael.kopp2@de.bosch.com>
2024-11-18 15:19:21 +0100

    hide number images by default and show on click

--- src/app.rs
+++ src/app.rs
@@ -31,20 +31,39 @@ pub fn app() -> Html {
     let number1 = 10 * digits[0] + digits[1];
     let number2 = 10 * digits[1] + digits[0];
 
+    let show_numbers_as_images = use_state(|| false);
+
+    let do_show_numbers_as_images = {
+        let show_numbers_as_images = show_numbers_as_images.clone();
+        move |_| {
+            show_numbers_as_images.set(true);
+        }
+    };
+
     html! {
         <main>
             <h1>{ "Welche Zahl ist größer?" }</h1>
             <div style="display: flex; justify-content: center; align-items: center; width: 80%; margin: 0 auto;">
-                <button style="font-size: 2em; width: 40%; margin: 0 10px;">{ number1 }</button>
-                <button style="font-size: 2em; width: 40%; margin: 0 10px;">{ number2 }</button>
+                <button style="font-size: 2em; width: 40%; margin: 0 10px; background-color: yellow;">{ number1 }</button>
+                <button style="font-size: 2em; width: 40%; margin: 0 10px; background-color: blue;">{ number2 }</button>
             </div>
-            <div style="margin-top: 1em;">
-                <button style="font-size: 2em; width: 80%;">{ "Zeige als Bilder" }</button>
-            </div>
-            <div style="display: flex; justify-content: center; align-items: center; width: 80%; margin: 0 auto;">
-                { number(number1, "yellow".to_string() ) }
-                { number(number2, "blue".to_string() ) }
-            </div>
+            {
+                if *show_numbers_as_images {
+                    html! {
+                        <div style="display: flex; justify-content: center; align-items: center; width: 80%; margin: 0 auto;">
+                            { number(number1, "yellow".to_string() ) }
+                            { number(number2, "blue".to_string() ) }
+                        </div>
+                    }
+                }
+                else {
+                    html! {
+                        <div style="margin-top: 1em;">
+                            <button onclick={do_show_numbers_as_images} style="font-size: 2em; width: 80%;">{ "Zeige als Bilder" }</button>
+                        </div>
+                    }
+                }
+            }
         </main>
     }
 }
